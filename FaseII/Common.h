6aa6ae1ab695110b81ab8a58914f14c7d7f72b3f
JavideSs <javimelladoo@gmail.com>
2021-03-10 18:57:51 +0100

    del files inncesarios

--- FaseII/Common.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-struct Result {
-    float m;
-    float n;
-};
--- FaseII/RegresionCpp.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "Common.h"
-
-Result regresionCpp(const float X[], const float Y[], int size);
