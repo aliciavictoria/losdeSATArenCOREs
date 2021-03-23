834381cba65825224e21886c07a88f547b3ea6ba
JavideSs <javimelladoo@gmail.com>
2021-03-23 15:38:59 +0100

    Include plplot

--- FaseII/FaseII.cpp
+++ FaseII/FaseII.cpp
@@ -4,6 +4,34 @@
 using std::cout;
 using std::endl;
 
+//___________________________________________________________________________
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include "plplot\plstream.h"
+using namespace std;
+const int NSIZE = 101;
+void test_plplot() {
+    PLFLT x[NSIZE], y[NSIZE];
+    PLFLT xmin = 0., xmax = 1., ymin = 0., ymax = 100.;
+    int   i;
+    for (i = 0; i < NSIZE; i++) {
+        x[i] = (PLFLT)(i) / (PLFLT)(NSIZE - 1);
+        y[i] = ymax * x[i] * x[i];
+    }
+    auto pls = new plstream();
+    plsdev("wingcc");
+    pls->init();
+    pls->env(xmin, xmax, ymin, ymax, 0, 0);
+    pls->lab("x", "y=100 x#u2#d", "Simple PLplot demo of a 2D line plot");
+    pls->line(NSIZE, x, y);
+    delete pls;
+}
+//___________________________________________________________________________
+
+
 int main(){
 	constexpr int arr_size = 5;
 	int* X = new int[arr_size] {1,2,6,4,5};
@@ -15,5 +43,6 @@ int main(){
 	Result resultx86 = regresionx86(X, Y, arr_size);
 	cout <<"Recta Regresion en x86-> m="<<resultx86.m<<", n="<<resultx86.n<< endl;
 
+    test_plplot();
 	return 0;
 }
